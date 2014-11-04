619a7706e8fcd4f85ea7e247af530080abc91007
BayuKurniaPutra <basink.bayu@gmail.com>
2014-11-04 13:30:02 +0700

    versi 2.3.2

---
+++ src/id/blist/luas/persegi.java
@@ -0,0 +1,11 @@
+package id.blist.luas;
+public class persegi 
+{
+  int sisi,luas;  
+  
+  void HitungPersegi(){
+      luas = sisi*sisi;
+      
+      System.out.print("Luas = Sisi(5) x Sisi(5) = "+luas);
+  }
+}
